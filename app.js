b5a39084624d3756bdb017dccd6aad28079ed138
Daniel Macedo <danielmsv2011@hotmail.com>
2022-08-28 10:09:18 -0300

    Connection with Database and Credentials

--- app.js
+++ app.js
@@ -9,4 +9,15 @@ app.get('/', (req, res) => {
     res.send("Funcionando!")
 })
 
-app.listen(3000, () => console.log("Servidor rodando"))
+
+// Credentials
+const dbUser = process.env.USER
+const dbPassword = process.env.PASSWORD
+const port = process.env.PORT
+
+
+// Connection with database
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.tibo4ql.mongodb.net/?retryWrites=true&w=majority`).then(() => {
+    console.log('Banco de dados conectado!')
+    app.listen(port, () => console.log("Servidor rodando na porta 4000"))
+})
