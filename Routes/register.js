43800b5a97f9fe4cdb6dcdb3ed34ed3c4234aef5
Daniel Macedo <danielmsv2011@hotmail.com>
2022-08-29 15:06:00 -0300

    register route finished

--- Routes/register.js
+++ Routes/register.js
@@ -1,43 +1,56 @@
 // register route
-
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
 
-const User = require('../Models/User');
+const User = require("../Models/User");
 
-router.post("/register", async (req, res) => {
-    const { name, email, password, confirmPassword } = req.body;
-  
-    if (!name) {
-      res.status(422).json({ message: "O nome é obrigatório!" });
-    }
-    if (!email) {
-      res.status(422).json({ message: "O email é obrigatório!" });
-    }
-    if (!password) {
-      res.status(422).json({ message: "A senha é obrigatória!" });
-    }
-    if (!confirmPassword) {
-      res.status(422).json({ message: "A confirmação de senha é obrigatória!" });
-    }
-    if (password !== confirmPassword) {
-      res.status(422).json({ message: "As senhas não correspondem a mesma!" });
-    }
-  
-    const userExist = await User.findOne({ email: email });
-  
-    if (userExist) {
-      res
-        .status(400)
-        .json({ message: "Esse email já esta em uso, tente novamente!" });
-    }
-  
-    // creating password
-    const salt = await bcrypt.genSalt(15);
-    const passwordHash = await bcrypt.hash(password, salt);
-  
-    const user = new User({
-      name,
-      email,
-      password: passwordHash,
-    });
+router.post("/", async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body;
+
+  if (!name) {
+    res.status(422).json({ message: "O nome é obrigatório!" });
+  }
+  if (!email) {
+    res.status(422).json({ message: "O email é obrigatório!" });
+  }
+  if (!password) {
+    res.status(422).json({ message: "A senha é obrigatória!" });
+  }
+  if (!confirmPassword) {
+    res.status(422).json({ message: "A confirmação de senha é obrigatória!" });
+  }
+  if (password !== confirmPassword) {
+    res.status(422).json({ message: "As senhas não correspondem a mesma!" });
+  }
+
+  const userExist = await User.findOne({ email: email });
+
+  if (userExist) {
+    res
+      .status(400)
+      .json({ message: "Esse email já esta em uso, tente novamente!" });
+  }
+
+  // creating password
+  const salt = await bcrypt.genSalt(15);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const user = new User({
+    name,
+    email,
+    password: passwordHash,
   });
+
+  try{
+
+    await user.save();
+
+    return res.status(201).json({ message: "Usuário cadastrado com sucesso!" })
+
+  } catch(error) {
+    console.log(error)
+    return res.status(500).json({ message: "Ocorreu um erro no servidor, tente novamente mais tarde!"})
+  }
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
 
 const app = express();
 
