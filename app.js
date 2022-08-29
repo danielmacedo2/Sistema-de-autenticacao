92f1b118b915267119ee80541208faee99ba2891
Daniel Macedo <danielmsv2011@hotmail.com>
2022-08-29 07:19:55 -0300

    register validations

--- app.js
+++ app.js
@@ -5,19 +5,47 @@ const jwt = require("jsonwebtoken");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Funcionando!")
-})
+// json configuration
+app.use(express.json())
 
+// open route
+app.get("/", (req, res) => {
+  res.send("Funcionando!");
+});
+
+// register
+app.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+
+  if(!name) {
+    res.status(422).json({ message: "O nome é obrigatório!" })
+  }
+  if(!email) {
+    res.status(422).json({ message: "O email é obrigatório!" })
+  }
+  if(!password) {
+    res.status(422).json({ message: "A senha é obrigatórias!" })
+  }
+  if(password !== confirmPassword) {
+    res.status(422).json({ message: "As senhas não correspondem a mesma!" })
+  }
+  if(!confirmPassword) {
+    res.status(422).json({ message: "A confirmação de senha é obrigatória!" })
+  }
+
+})
 
 // Credentials
-const dbUser = process.env.USER
-const dbPassword = process.env.PASSWORD
-const port = process.env.PORT
-
+const dbUser = process.env.USER;
+const dbPassword = process.env.PASSWORD;
+const port = process.env.PORT;
 
 // Connection with database
-mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.tibo4ql.mongodb.net/?retryWrites=true&w=majority`).then(() => {
-    console.log('Banco de dados conectado!')
-    app.listen(port, () => console.log("Servidor rodando na porta 4000"))
-})
+mongoose
+  .connect(
+    `mongodb+srv://${dbUser}:${dbPassword}@cluster0.tibo4ql.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Banco de dados conectado!");
+    app.listen(port, () => console.log("Servidor rodando na porta 4000"));
+  });
