14ca787ed3cde48ce985bc0f03a750a06b96586f
Daniel Macedo <danielmsv2011@hotmail.com>
2022-08-29 15:16:42 -0300

    checando se usuário existe

--- Routes/login.js
+++ Routes/login.js
@@ -5,9 +5,22 @@ const bcrypt = require("bcrypt");
 const User = require("../Models/User");
 
 router.post("/", async (req, res) => {
-  const { name, email, password } = req.body;
+  const { email, password } = req.body;
 
-  
+  if(!email) {
+    return res.status(422).json({ message: "O email é obrigatório!" })
+  }
+  if(!password) {
+    return res.status(422).json({ message: "A senha é obrigatória!" })
+  }
+
+  const userExist = await User.findOne({ email: email })
+
+  if(!userExist) {
+    return res.status(404).json({ message: "Usuário não encontrado, email inválido!"})
+  }
+
+  // conferindo a senha
 })
 
 module.exports = router;
